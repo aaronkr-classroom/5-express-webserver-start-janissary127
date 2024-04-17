0adfd5c97f8946218bf4938e93ec827b2dd90d31
janissary127 <88474405+janissary127@users.noreply.github.com>
2024-04-17 01:48:41 +0000

    10, 11 Finish

--- lesson-11/main.js
+++ lesson-11/main.js
@@ -2,3 +2,25 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs");
+
+app.use(layouts);
+app.use(express.static("public")); // 정적파일 디렉토리
+
+app.get('/name/:myName', homeController.respondWithName);
+
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound);
+app.use(errorController.resInternalError);
+
+app.listen(app.get("port"), () => {
+    console.log(`Server at http://localhost:${app.get("port")}`);
+});
